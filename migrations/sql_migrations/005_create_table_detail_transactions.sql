a0f618ddb867a2e858563ee619dced2c2746924d
fahmirzl <fahmirizal.dev@gmail.com>
2025-06-17 22:44:02 +0700

    refactor(table): add transaction_id column on transaction_details table

--- migrations/sql_migrations/005_create_table_detail_transactions.sql
+++ migrations/sql_migrations/005_create_table_detail_transactions.sql
@@ -3,6 +3,7 @@
 
 CREATE TABLE transaction_details (
     id INT PRIMARY KEY AUTO_INCREMENT,
+    transaction_id INT NOT NULL,
     product_id INT NOT NULL,
     purchase_price INT NOT NULL,
     selling_price INT NOT NULL,
@@ -10,7 +11,8 @@ CREATE TABLE transaction_details (
     subtotal INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES products(id)
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (transaction_id) REFERENCES transactions(id)
 )
 
 -- +migrate StatementEnd
