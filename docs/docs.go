4b8714817301b278087bbddeed1e726483ed39e5
fahmirzl <fahmirizal.dev@gmail.com>
2025-06-21 20:26:19 +0700

    (info): forgot swag init command

--- docs/docs.go
+++ docs/docs.go
@@ -1112,7 +1112,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "https://casheex-production.up.railway.app",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Casheex",
